4c69eb50749562e6eac93804267d00557a02f498
benjamin <benjamin@510b7424-7745-11de-94da-c3e9e1814b9e>
2007-11-22 13:02:33 +0000

    # DotNet Helper

---
+++ Libraries/DotNet/CompactFramework/2.0/Helpers/System.Windows.Forms.vb
@@ -0,0 +1,25 @@
+
+
+imports System.ComponentModel
+
+Namespace System.Windows.Forms
+	
+	Public Class PictureBox
+		inherits Global.System.Windows.Forms.PictureBox
+		
+		Implements ISupportInitialize
+		
+		Public Sub BeginInit Implements ISupportInitialize.BeginInit 			
+		End Sub
+		
+		Public Sub EndInit Implements ISupportInitialize.EndInit 			
+		End Sub
+		
+		
+		Public Shadows TabIndex As Int32
+		
+		Public Shadows TabStop As Boolean
+				 		
+	End Class
+	
+End Namespace
