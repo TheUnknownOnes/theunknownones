322a80cf25a8536b4c255774df66ef1f4a1f342b
benjamin <benjamin@510b7424-7745-11de-94da-c3e9e1814b9e>
2007-11-23 06:15:20 +0000

    + C# :)

---
+++ Libraries/DotNet/CompactFramework/2.0/Helpers/TUO.CompactFramework.2.Helpers/System.Windows.Forms.cs
@@ -0,0 +1,161 @@
+
+using System;
+using System.Drawing;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace TUO.System.Windows.Forms
+{
+	
+	
+	public class Button : global::System.Windows.Forms.Button
+  {
+  	public String AccessibleDefaultActionDescription;
+		public String AccessibleDescription;
+		public String AccessibleName;
+		public Boolean AllowDrop;
+		public Boolean AutoEllipsis;
+		public Point AutoScrollOffset;
+		public Boolean AutoSize;
+		public Image BackgroundImage;
+		public Boolean CanFocus;
+		public Boolean CanSelect;
+		public Boolean CausesValidation;
+		public Boolean CheckForIllegalCrossThreadCalls;
+		public String CompanyName;
+		public IContainer Container;
+		public Boolean ContainsFocus;
+		public Boolean Created;
+		public Cursor Cursor;
+		public Color DefaultBackColor;
+		public Font DefaultFont;
+		public Color DefaultForeColor;
+		public Rectangle DisplayRectangle;
+		public Boolean Disposing;
+		public Boolean FlatAppearance;
+		public Boolean HasChildren;
+		public Image Image;
+		public ContentAlignment ImageAlign;
+		public Int32 ImageIndex;
+		public String ImageKey;
+		public ImageList ImageList;
+		public Boolean IsAccessible;
+		public Boolean IsDisposed;
+		public Boolean IsHandleCreated;
+		public Boolean IsMirrored;
+		public Size MaximumSize;
+		public Size MinimumSize;
+		public Size PreferredSize;
+		public String ProductName;
+		public String ProductVersion;
+		public Boolean RecreatingHandle;
+		public Region Region;
+		public ContentAlignment	 TextAlign;
+		public Boolean UseCompatibleTextRendering;
+		public Boolean UseMnemonic;
+		public Boolean UseVisualStyleBackColor;
+		public Boolean UseWaitCursor;
+  }
+  
+	
+	
+	public class Label : global::System.Windows.Forms.Label
+	{
+		public String AccessibleDefaultActionDescription;
+		public String AccessibleDescription;
+		public String AccessibleName;
+		public Boolean AllowDrop;
+		public Boolean AutoEllipsis;
+		public Point AutoScrollOffset;
+		public Boolean AutoSize;
+		public Image BackgroundImage;
+		public BorderStyle BorderStyle;
+		public Boolean CanFocus;
+		public Boolean CanSelect;
+		public Boolean CausesValidation;
+		public Boolean CheckForIllegalCrossThreadCalls;
+		public String CompanyName;
+		public IContainer Container;
+		public Boolean ContainsFocus;
+		public Boolean Created;
+		public Cursor Cursor;
+		public Color DefaultBackColor;
+		public Font DefaultFont;
+		public Color DefaultForeColor;
+		public Rectangle DisplayRectangle;
+		public Boolean Disposing;
+		public Boolean HasChildren;
+		public Image Image;
+		public ContentAlignment ImageAlign;
+		public Int32 ImageIndex;
+		public String	 ImageKey;
+		public Boolean IsAccessible;
+		public Boolean IsDisposed;
+		public Boolean IsHandleCreated;
+		public Boolean IsMirrored;
+		public Size MaximumSize;
+		public Size MinimumSize;
+		public Int32 PreferredHeight;
+		public Size PreferredSize;
+		public Int32 PreferredWidth;
+		public String ProductName;
+		public String ProductVersion;
+		public Boolean RecreatingHandle;
+		public Region Region;
+		public new Int32 TabIndex;
+		public new Boolean TabStop;
+		public Boolean UseCompatibleTextRendering;
+		public Boolean UseMnemonic;
+		public Boolean UseWaitCursor;	
+	}
+	
+	
+	public class PictureBox : global::System.Windows.Forms.PictureBox
+	{
+		public String AccessibleDefaultActionDescription;
+		public String AccessibleDescription;
+		public String AccessibleName;
+		public String AccessibleRole;
+		public Boolean AllowDrop;
+		public Point AutoScrollOffset;
+		public Boolean AutoSize;
+		public Image BackgroundImage;
+		public BorderStyle BorderStyle;
+		public Boolean CanFocus;
+		public Boolean CanSelect;
+		public Boolean CausesValidation;
+		public Boolean CheckForIllegalCrossThreadCalls;
+		public String CompanyName;
+		public IContainer Container;
+		public Boolean ContainsFocus;
+		public Boolean Created;
+		public Cursor Cursor;
+		public Color DefaultBackColor;
+		public Font DefaultFont;
+		public Color DefaultForeColor;
+		public Rectangle DisplayRectangle;
+		public Boolean Disposing;
+		public Image ErrorImage;
+		public Boolean HasChildren;
+		public String ImageLocation;
+		public Image InitialImage;
+		public Boolean IsAccessible;
+		public Boolean IsDisposed;
+		public Boolean IsHandleCreated;
+		public Boolean IsMirrored;
+		public Size MaximumSize;
+		public Size MinimumSize;
+		public Keys ModifierKeys;
+		public Size PreferredSize;
+		public String ProductName;
+		public String ProductVersion;
+		public Boolean RecreatingHandle;
+		public Region Region;
+		public new Int32 TabIndex;
+		public new Boolean TabStop;
+		public Boolean UseWaitCursor;
+		public Boolean WaitOnLoad;
+	}
+		
+	
+}
