1913d21f7f16f22860cbda06089edd4530b43642
MarcusCalidus <marcuscalidus@gmail.com>
2009-10-23 08:52:47 +0000

    Minor changes and bugfixes from trunk r444

---
+++ CustomHelp/1.2.1/uFrameConfig3rdParty.dfm
@@ -0,0 +1,29 @@
+object FrameConfig3rdParty: TFrameConfig3rdParty
+  Left = 0
+  Top = 0
+  Width = 601
+  Height = 44
+  TabOrder = 0
+  object GroupBox1: TGroupBox
+    Left = 0
+    Top = 0
+    Width = 601
+    Height = 44
+    Align = alClient
+    Caption = 'GroupBox1'
+    TabOrder = 0
+    inline FrameConfigColor1: TFrameConfigColor
+      Left = 2
+      Top = 15
+      Width = 597
+      Height = 22
+      Align = alTop
+      AutoSize = True
+      TabOrder = 0
+      TabStop = True
+      ExplicitLeft = 2
+      ExplicitTop = 15
+      ExplicitWidth = 597
+    end
+  end
+end
---
+++ CustomHelp/1.2.1/uFrameConfig3rdParty.pas
@@ -0,0 +1,70 @@
+unit uFrameConfig3rdParty;
+
+interface
+
+uses
+  Windows,
+  Messages,
+  SysUtils,
+  Variants,
+  Classes,
+  Graphics,
+  Controls,
+  Forms,
+  Dialogs,
+  StdCtrls,
+  uFrameConfigColor;
+
+type
+  TFrameConfig3rdParty = class(TFrame)
+    FrameConfigColor1: TFrameConfigColor;
+    GroupBox1:         TGroupBox;
+  private
+    function GetColor: TColor;
+    function GetProviderName: string;
+    procedure SetColor(const Value: TColor);
+    procedure SetProviderName(const Value: string);
+    { Private-Deklarationen }
+  public
+    property ProviderName: string read GetProviderName write SetProviderName;
+    property SelectedColor: TColor read GetColor write SetColor;
+
+    procedure Save;
+  end;
+
+implementation
+
+uses
+  uCustomHelpMain;
+
+{$R *.dfm}
+
+{ TFrame1 }
+
+function TFrameConfig3rdParty.GetColor: TColor;
+begin
+  Result := FrameConfigColor1.SelectedColor;
+end;
+
+function TFrameConfig3rdParty.GetProviderName: string;
+begin
+  Result := GroupBox1.Caption;
+end;
+
+procedure TFrameConfig3rdParty.Save;
+begin
+  GlobalCustomHelp.Color[ProviderName] := SelectedColor;
+end;
+
+procedure TFrameConfig3rdParty.SetColor(const Value: TColor);
+begin
+  FrameConfigColor1.SelectedColor := Value;
+end;
+
+procedure TFrameConfig3rdParty.SetProviderName(const Value: string);
+begin
+  GroupBox1.Caption := Value;
+  FrameConfigColor1.catbtnTopics.Categories[0].Caption := Value;
+end;
+
+end.
