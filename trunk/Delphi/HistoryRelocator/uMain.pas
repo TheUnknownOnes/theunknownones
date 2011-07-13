322af4f92ac5f000c1f67f37c3f9d3cf8c9a8857
chaosben@gmail.com <chaosben@gmail.com@510b7424-7745-11de-94da-c3e9e1814b9e>
2011-07-13 04:23:25 +0000

    # added subdirs to HistoryPath

--- trunk/Delphi/HistoryRelocator/uMain.pas
+++ trunk/Delphi/HistoryRelocator/uMain.pas
@@ -16,7 +16,7 @@ uses
   Menus,
   Registry,
   StrUtils,
-  Dialogs,
+  ImageHlp,
   uHookTools;
 
 type
@@ -36,7 +36,7 @@ type
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
 
-    procedure RelocPath(ASource : String; var APath : String);
+    function RelocatePath(APath : String) : String;
 
     property RTL_BPL_Handle : Cardinal read FRTL_BPL_Handle;
     property COREIDE_BPL_Handle : Cardinal read FCOREIDE_BPL_Handle;
@@ -83,69 +83,44 @@ var
 
   _OrigGetFileAttributesW : function(lpFileName: PWideChar): DWORD; stdcall;
 
+  _OrigCreateDirectoryW : function (lpPathName: PWideChar; lpSecurityAttributes: PSecurityAttributes): BOOL; stdcall;
+
 
 function _NewCreateFileW(lpFileName: PWideChar; dwDesiredAccess, dwShareMode: DWORD;
                         lpSecurityAttributes: PSecurityAttributes; dwCreationDisposition, dwFlagsAndAttributes: DWORD;
                         hTemplateFile: THandle): THandle; stdcall;
-var
-  s : String;
+
 begin
-  s := lpFileName;
-
-  HistoryRelocator.RelocPath('CreateFileW', s);
-
-  Result := _OrigCreateFileW(PWideChar(WideString(s)), dwDesiredAccess, dwShareMode,
+  Result := _OrigCreateFileW(PWideChar(WideString(HistoryRelocator.RelocatePath(lpFileName))), dwDesiredAccess, dwShareMode,
                              lpSecurityAttributes, dwCreationDisposition,
                              dwFlagsAndAttributes, hTemplateFile);
 end;
 
 function _NewMoveFileW(lpExistingFileName, lpNewFileName: PWideChar): BOOL; stdcall;
-var
-  s : String;
 begin
-  s := lpNewFileName;
-
-  HistoryRelocator.RelocPath('MoveFileW', s);
-
-  Result := _OrigMoveFileW(lpExistingFileName, PWideChar(WideString(s)));
+  Result := _OrigMoveFileW(lpExistingFileName, PWideChar(WideString(HistoryRelocator.RelocatePath(lpNewFileName))));
 end;
 
 function _NewFindFirstFileW(lpFileName: PWideChar; var lpFindFileData: TWIN32FindDataW): THandle; stdcall;
-var
-  s : String;
 begin
-  s := lpFileName;
-
-  HistoryRelocator.RelocPath('FindFirstFileW', s);
-
-  Result := _OrigFindFirstFileW(PWideChar(WideString(s)), lpFindFileData);
+  Result := _OrigFindFirstFileW(PWideChar(WideString(HistoryRelocator.RelocatePath(lpFileName))), lpFindFileData);
 end;
 
 function _NewGetFileSecurityW(lpFileName: PWideChar; RequestedInformation: SECURITY_INFORMATION;
   pSecurityDescriptor: PSecurityDescriptor; nLength: DWORD; var lpnLengthNeeded: DWORD): BOOL; stdcall;
-var
-  s : String;
 begin
-  s := lpFileName;
-
-  HistoryRelocator.RelocPath('GetFileSecurityW', s);
-
-  Result := _OrigGetFileSecurityW(PWideChar(WideString(s)), RequestedInformation, pSecurityDescriptor, nLength, lpnLengthNeeded);
+  Result := _OrigGetFileSecurityW(PWideChar(WideString(HistoryRelocator.RelocatePath(lpFileName))), RequestedInformation, pSecurityDescriptor, nLength, lpnLengthNeeded);
 end;
 
 function _NewGetFileAttributesW(lpFileName: PWideChar): DWORD; stdcall;
-var
-  s : String;
 begin
-  s := lpFileName;
-
-  HistoryRelocator.RelocPath('GetFileAttributesW', s);
-
-  Result := _OrigGetFileAttributesW(PWideChar(WideString(s)));
+  Result := _OrigGetFileAttributesW(PWideChar(WideString(HistoryRelocator.RelocatePath(lpFileName))));
 end;
 
-
-
+function _NewCreateDirectoryW(lpPathName: PWideChar; lpSecurityAttributes: PSecurityAttributes): BOOL; stdcall;
+begin
+  Result := _OrigCreateDirectoryW(PWideChar(WideString(HistoryRelocator.RelocatePath(lpPathName))), lpSecurityAttributes);
+end;
 
 { THistoryRelocator }
 
@@ -153,19 +128,18 @@ constructor THistoryRelocator.Create(AOwner: TComponent);
 begin
   inherited;
 
-  //(BorlandIDEServices as IOTAMessageServices).ClearAllMessages;
-
-
   LoadSettings;
 
   FRTL_BPL_Handle := GetModuleHandle(RTL_BPL_Filename);
   FCOREIDE_BPL_Handle := GetModuleHandle(COREIDE_BPL_Filename);
+
 
   HookImport(FRTL_BPL_Handle, 'kernel32.dll', 'CreateFileW', @_NewCreateFileW, @_OrigCreateFileW);
   HookImport(FRTL_BPL_Handle, 'kernel32.dll', 'MoveFileW', @_NewMoveFileW, @_OrigMoveFileW);
   HookImport(FRTL_BPL_Handle, 'kernel32.dll', 'FindFirstFileW', @_NewFindFirstFileW, @_OrigFindFirstFileW);
   //HookImport(FCOREIDE_BPL_Handle, 'advapi32.dll', 'GetFileSecurityW', @_NewGetFileSecurityW, @_OrigGetFileSecurityW);
   HookImport(FRTL_BPL_Handle, 'kernel32.dll', 'GetFileAttributesW', @_NewGetFileAttributesW, @_OrigGetFileAttributesW);
+  HookImport(FRTL_BPL_Handle, 'kernel32.dll', 'CreateDirectoryW', @_NewCreateDirectoryW, @_OrigCreateDirectoryW);
 
 
   FConfigMenu := TMenuItem.Create(self);
@@ -185,6 +159,7 @@ begin
   UnHookImport(FRTL_BPL_Handle, 'kernel32.dll', 'FindFirstFileW', @_NewFindFirstFileW, @_OrigFindFirstFileW);
  // UnHookImport(FCOREIDE_BPL_Handle, 'advapi32.dll', 'GetFileSecurityW', @_NewGetFileSecurityW, @_OrigGetFileSecurityW);
   UnHookImport(FRTL_BPL_Handle, 'kernel32.dll', 'GetFileAttributesW', @_NewGetFileAttributesW, @_OrigGetFileAttributesW);
+  UnHookImport(FRTL_BPL_Handle, 'kernel32.dll', 'CreateDirectoryW', @_NewCreateDirectoryW, @_OrigCreateDirectoryW);
 
   inherited;
 end;
@@ -204,6 +179,7 @@ begin
   Result := TRegistry.Create;
   Result.RootKey := HKEY_CURRENT_USER;
   Result.OpenKey(GetTUOCommon.RegistryRootKey + '\HistoryRelocator', true);
+
 end;
 
 procedure THistoryRelocator.LoadSettings;
@@ -215,20 +191,23 @@ begin
   end;
 end;
 
-procedure THistoryRelocator.RelocPath(ASource : String; var APath: String);
+
+function THistoryRelocator.RelocatePath(APath: String) : String;
 const
   HistFolder = '__history';
   HistFileExt = '.~*~';
+
 begin
+  Result := APath;
   if HistoryPath = '' then exit;
   if not ContainsText(APath, HistFolder) then exit;
 
-  if not DirectoryExists(HistoryPath) then
-    CreateDir(HistoryPath);
+  if APath[2] = ':' then
+    APath := Copy(APath, 3, Length(APath));
 
-  APath := HistoryPath + ExtractFileName(APath);
+  Result := HistoryPath + APath;
 
-  //(BorlandIDEServices as IOTAMessageServices).AddWideTitleMessage('[' + ASource + '] ' + APath);
+  MakeSureDirectoryPathExists(PAnsiChar(AnsiString(ExtractFilePath(Result))));
 end;
 
 
